afdec9efa0a2450df154285b554f30ae30dcd3ba
SamuelKauffman <112411760+SamuelKauffman@users.noreply.github.com>
2022-09-06 15:13:48 -0400

    Add files via upload

--- TakeHomeAssignment.java
+++ TakeHomeAssignment.java
@@ -6,8 +6,5 @@ public class TakeHomeAssignment
 		System.out.println("My last name is : " + args[1]);
 		System.out.println("My hobby is : " + args[2]);
 		System.out.println("My major is : " + args[3]);
-		double number1 = 88.22;
-		int number2 = 45;
-		System.out.println(number1 * number2);
 	}
 }	
---
+++ TakeHomeSum.java
@@ -0,0 +1,9 @@
+public class TakeHomeSum
+{
+	public static void main(String[] args)
+	{
+		double a = Double.parseDouble(args[0]);
+		Integer b= Integer.parseInt(args [1]);
+		System.out.println("Multiplication = " + (a*b));
+	}
+}
